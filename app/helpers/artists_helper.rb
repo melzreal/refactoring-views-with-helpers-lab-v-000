e9d901134f636e990aa227a2aecfd83821cc657a
Melissa Real <melissa.real@hotmail.com>
2019-02-07 18:48:47 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,11 @@
 module ArtistsHelper
 
   def display_artist(song)
-    song.artist? ? link_to song.artist_name, artist_path(song.artist) : link_to "Add Artist", edit_song_path(song)
+    if song.artist
+     link_to song.artist_name, artist_path(song.artist)
+   else
+      link_to "Add Artist", edit_song_path(song)
+    end 
   end
 
 end
