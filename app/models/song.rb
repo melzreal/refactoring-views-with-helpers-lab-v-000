d144527fcba8f65db0743c0f8ca45b342f91617c
Melissa Real <melissa.real@hotmail.com>
2019-02-07 18:43:49 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist ? self.artist.name
+   self.artist.name unless !self.artist
   end
 
   def artist_name=(name)
