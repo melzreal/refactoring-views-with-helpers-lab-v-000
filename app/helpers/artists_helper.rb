7149a607dbf4f4171d5ffdefbe1ed9743a53b880
Melissa Real <melissa.real@hotmail.com>
2019-02-07 17:58:35 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,14 @@
 module ArtistsHelper
+
+  def display_artist(song)
+
+    x = Song.find_by(title: song)
+
+    if !x.artist.name
+      redirect "/'#{x.id}'/edit"
+    else
+      redirect "/'#{x.artist.id}'/show"
+    end
+  end
+
 end
